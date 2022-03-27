b404880c9119e48ba9e183249995d1927091da80
hsshin <hyunseo99999@naver.com>
2022-03-27 17:38:04 +0900

    인증부가기능 처리

--- src/main/java/com/security/security/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/security/security/provider/CustomAuthenticationProvider.java
@@ -1,16 +1,20 @@
 package com.security.security.provider;
 
+import com.security.security.common.FormWebAuthenticationDetails;
 import com.security.security.service.AccountContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
@@ -29,6 +33,12 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         if (!passwordEncoder.matches(password, accountContext.getPassword())) {
             throw new BadCredentialsException("BadCredentialsException");
         }
+
+        FormWebAuthenticationDetails details = (FormWebAuthenticationDetails) authentication.getDetails();
+        if (details.getSecret_key() != null && !"secret".equals(details.getSecret_key())) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
+        }
+
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
         return authenticationToken;
